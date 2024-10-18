1b3cba9835bf7d55d350781d509f9878a176a8dc
Юлия Яцынина <julisa.milina@gmail.com>
2024-10-18 16:07:26 +0700

    изменения

--- discriminant.py
+++ discriminant.py
@@ -24,21 +24,13 @@ def main():
 
 def test_calculate_discriminant():
     # Положительные тесты
-    run_test(1, -3, 2, 1, "Test case 1 failed")
-    run_test(1, 0, -4, 16, "Test case 2 failed")
-    run_test(1, 2, 1, 0, "Test case 3 failed")
+    assert calculate_discriminant(1, -3, 2) == 1, "Test case 1 failed"
+    assert calculate_discriminant(1, 0, -4) == 16, "Test case 2 failed"
+    assert calculate_discriminant(1, 2, 1) == 0, "Test case 3 failed"
 
     # Негативные тесты
-    run_test(1, 1, 1, -3, "Test case 4 failed")
-    run_test(1, 0, 1, -4, "Test case 5 failed")
-
-    print("Все тесты пройдены успешно!")
-
-
-def run_test(a, b, c, expected, message):
-    result = calculate_discriminant(a, b, c)
-    if result != expected:
-        raise AssertionError(message)
+    assert calculate_discriminant(1, 1, 1) == -3, "Test case 4 failed"
+    assert calculate_discriminant(1, 0, 1) == -4, "Test case 5 failed"
 
 
 if __name__ == "__main__":
