c1441d7c02d01c8432f28d7f68ac7cd81971068c
Юлия Яцынина <julisa.milina@gmail.com>
2024-10-18 15:55:21 +0700

    изменения

--- discriminant.py
+++ discriminant.py
@@ -22,22 +22,25 @@ def main():
         print("Нет вещественных корней.")
 
 
-if __name__ == "__main__":
-    main()
-
-
 def test_calculate_discriminant():
     # Положительные тесты
-    assert calculate_discriminant(1, -3, 2) == 1, "Test case 1 failed"
-    assert calculate_discriminant(1, 0, -4) == 16, "Test case 2 failed"
-    assert calculate_discriminant(1, 2, 1) == 0, "Test case 3 failed"
+    run_test(1, -3, 2, 1, "Test case 1 failed")
+    run_test(1, 0, -4, 16, "Test case 2 failed")
+    run_test(1, 2, 1, 0, "Test case 3 failed")
 
     # Негативные тесты
-    assert calculate_discriminant(1, 1, 1) == -3, "Test case 4 failed"
-    assert calculate_discriminant(1, 0, 1) == -4, "Test case 5 failed"
+    run_test(1, 1, 1, -3, "Test case 4 failed")
+    run_test(1, 0, 1, -4, "Test case 5 failed")
 
     print("Все тесты пройдены успешно!")
 
 
+def run_test(a, b, c, expected, message):
+    result = calculate_discriminant(a, b, c)
+    if result != expected:
+        raise AssertionError(message)
+
+
 if __name__ == "__main__":
+    main()
     test_calculate_discriminant()
